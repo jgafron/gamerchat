63974db0438cb9ae1e9d2aa20c097d1963cf0cb7
joey gafron <jgafron@pdx.edu>
2024-04-30 15:24:10 -0700

    Added login page background and logo

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,7 +4,6 @@ import Login from '../components/Login.jsx';
 function App() {
   return (
     <div>
-      <h1>GamerChat</h1>
       <Login /> {/* Render login*/}
     </div>
   );
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -24,31 +24,33 @@ function Login() {
   };
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+    <div>
+      <div className="login-container">
+      <img src="/src/images/gamerchat-logo.png" alt="Image" className="logo" />
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username" className ="username-input" >Username or Email</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={handleUsernameChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" className ="username-input">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 }
